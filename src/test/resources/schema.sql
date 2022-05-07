a8d5f53288c046b866774125cb9ba1c5d119b97a
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2022-05-07 18:22:02 +0200

    Update

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,13 +1,3 @@
-SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT;
-SET NAMES utf8;
-SET NAMES utf8mb4;
-SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
-SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
-SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0;
-
-CREATE DATABASE IF NOT EXISTS `my_garage`;
-USE `my_garage`;
-
 CREATE TABLE IF NOT EXISTS `permissions`
 (
     `id`         bigint(20) NOT NULL AUTO_INCREMENT,
@@ -17,9 +7,6 @@ CREATE TABLE IF NOT EXISTS `permissions`
   AUTO_INCREMENT = 7
   DEFAULT CHARSET = utf8;
 
-DELETE
-FROM `permissions`;
-
 INSERT INTO `permissions` (`id`, `permission`)
 VALUES (1, 'user:read'),
        (2, 'user:write'),
@@ -27,8 +14,6 @@ VALUES (1, 'user:read'),
        (4, 'support:write'),
        (5, 'admin:read'),
        (6, 'admin:write');
-ALTER TABLE `permissions`
-    ENABLE KEYS;
 
 CREATE TABLE IF NOT EXISTS `roles`
 (
@@ -39,16 +24,10 @@ CREATE TABLE IF NOT EXISTS `roles`
   AUTO_INCREMENT = 4
   DEFAULT CHARSET = utf8;
 
-DELETE
-FROM `roles`;
-ALTER TABLE `roles`
-    DISABLE KEYS;
 INSERT INTO `roles` (`id`, `name`)
 VALUES (1, 'USER'),
        (2, 'SUPPORT'),
        (3, 'ADMIN');
-ALTER TABLE `roles`
-    ENABLE KEYS;
 
 CREATE TABLE IF NOT EXISTS `role_permission`
 (
@@ -61,10 +40,6 @@ CREATE TABLE IF NOT EXISTS `role_permission`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-DELETE
-FROM `role_permission`;
-ALTER TABLE `role_permission`
-    DISABLE KEYS;
 INSERT INTO `role_permission` (`role_id`, `permission_id`)
 VALUES (1, 1),
        (2, 1),
@@ -78,8 +53,6 @@ VALUES (1, 1),
        (3, 4),
        (3, 5),
        (3, 6);
-ALTER TABLE `role_permission`
-    ENABLE KEYS;
 
 CREATE TABLE IF NOT EXISTS `users`
 (
@@ -99,10 +72,6 @@ CREATE TABLE IF NOT EXISTS `users`
   AUTO_INCREMENT = 4
   DEFAULT CHARSET = utf8;
 
-DELETE
-FROM `users`;
-ALTER TABLE `users`
-    DISABLE KEYS;
 INSERT INTO `users` (`id`, `email`, `is_account_non_expired`, `is_account_non_locked`, `is_credentials_non_expired`,
                      `is_enabled`, `password`, `username`, `role_id`)
 VALUES (1, 'test@gmail.com', b'1', b'1', b'1', b'1', '$2a$10$zTBMNrhpC3QvJRlogKYNz.rq2J7YUoZEBzuyGe5fYnPK3DbSilhAy',
@@ -111,10 +80,4 @@ VALUES (1, 'test@gmail.com', b'1', b'1', b'1', b'1', '$2a$10$zTBMNrhpC3QvJRlogKY
         'admin', 3),
        (3, 'test@gmail.com', b'0', b'0', b'0', b'1', '$2a$10$bpVNE6D2yBXCB7xVoC3nce.15XS0EvwcJNkctVCpD7LdzDzg15fOa',
         'supporter', 2);
-ALTER TABLE `users`
-    ENABLE KEYS;
 
-SET SQL_MODE = IFNULL(@OLD_SQL_MODE, '');
-SET FOREIGN_KEY_CHECKS = IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1);
-SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT;
-SET SQL_NOTES = IFNULL(@OLD_SQL_NOTES, 1);
