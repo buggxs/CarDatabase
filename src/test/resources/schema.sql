74f778e60f9421493f41abc28cb5039dfd529d64
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2022-05-07 18:23:21 +0200

    Update

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,12 +1,3 @@
-CREATE TABLE IF NOT EXISTS `permissions`
-(
-    `id`         bigint(20) NOT NULL AUTO_INCREMENT,
-    `permission` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 7
-  DEFAULT CHARSET = utf8;
-
 INSERT INTO `permissions` (`id`, `permission`)
 VALUES (1, 'user:read'),
        (2, 'user:write'),
@@ -15,30 +6,10 @@ VALUES (1, 'user:read'),
        (5, 'admin:read'),
        (6, 'admin:write');
 
-CREATE TABLE IF NOT EXISTS `roles`
-(
-    `id`   bigint(20) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4
-  DEFAULT CHARSET = utf8;
-
 INSERT INTO `roles` (`id`, `name`)
 VALUES (1, 'USER'),
        (2, 'SUPPORT'),
        (3, 'ADMIN');
-
-CREATE TABLE IF NOT EXISTS `role_permission`
-(
-    `role_id`       bigint(20) NOT NULL,
-    `permission_id` bigint(20) NOT NULL,
-    PRIMARY KEY (`role_id`, `permission_id`),
-    KEY `FK2xn8qv4vw30i04xdxrpvn3bdi` (`permission_id`),
-    CONSTRAINT `FK2xn8qv4vw30i04xdxrpvn3bdi` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`),
-    CONSTRAINT `FKtfgq8q9blrp0pt1pvggyli3v9` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
 
 INSERT INTO `role_permission` (`role_id`, `permission_id`)
 VALUES (1, 1),
@@ -53,24 +24,6 @@ VALUES (1, 1),
        (3, 4),
        (3, 5),
        (3, 6);
-
-CREATE TABLE IF NOT EXISTS `users`
-(
-    `id`                         bigint(20) NOT NULL AUTO_INCREMENT,
-    `email`                      varchar(255) DEFAULT NULL,
-    `is_account_non_expired`     bit(1)     NOT NULL,
-    `is_account_non_locked`      bit(1)     NOT NULL,
-    `is_credentials_non_expired` bit(1)     NOT NULL,
-    `is_enabled`                 bit(1)     NOT NULL,
-    `password`                   varchar(255) DEFAULT NULL,
-    `username`                   varchar(255) DEFAULT NULL,
-    `role_id`                    bigint(20)   DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `FKp56c1712k691lhsyewcssf40f` (`role_id`),
-    CONSTRAINT `FKp56c1712k691lhsyewcssf40f` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 4
-  DEFAULT CHARSET = utf8;
 
 INSERT INTO `users` (`id`, `email`, `is_account_non_expired`, `is_account_non_locked`, `is_credentials_non_expired`,
                      `is_enabled`, `password`, `username`, `role_id`)
