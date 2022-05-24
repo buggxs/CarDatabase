1d4b2472771fffa6a0980f7b27d2976270e8a792
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2022-05-24 17:09:05 +0200

    REMOVE test

--- src/test/java/de/buggxs/mygarage/CarIdentApplicationTests.java
+++ src/test/java/de/buggxs/mygarage/CarIdentApplicationTests.java
@@ -1,6 +1,5 @@
 package de.buggxs.mygarage;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class CarIdentApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.fail("Oops failed");
     }
 
 }
