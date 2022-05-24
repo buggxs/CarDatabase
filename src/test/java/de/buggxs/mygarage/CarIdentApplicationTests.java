e6df03de074624c93fe2a1eab301173f1f7de470
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2022-05-24 17:08:11 +0200

    ADD test

--- src/test/java/de/buggxs/mygarage/CarIdentApplicationTests.java
+++ src/test/java/de/buggxs/mygarage/CarIdentApplicationTests.java
@@ -1,13 +1,15 @@
 package de.buggxs.mygarage;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarIdentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.fail("Oops failed");
+    }
 
 }
