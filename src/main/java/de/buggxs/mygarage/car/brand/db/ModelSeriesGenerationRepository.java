dd83cc0c9a50a5dc486b63eab1faa4429f15e2a1
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2022-08-13 18:20:26 +0200

    ADD search model series and generation by name

--- src/main/java/de/buggxs/mygarage/car/brand/db/ModelSeriesGenerationRepository.java
+++ src/main/java/de/buggxs/mygarage/car/brand/db/ModelSeriesGenerationRepository.java
@@ -1,7 +1,13 @@
 package de.buggxs.mygarage.car.brand.db;
 
 import de.buggxs.mygarage.car.brand.ModelSeriesGeneration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ModelSeriesGenerationRepository extends PagingAndSortingRepository<ModelSeriesGeneration, Long> {
+
+    Page<ModelSeriesGeneration> findModelSeriesGenerationByNameContainingIgnoreCase(String name, Pageable pageable);
+
+
 }
--- src/main/java/de/buggxs/mygarage/car/brand/db/ModelSeriesRepository.java
+++ src/main/java/de/buggxs/mygarage/car/brand/db/ModelSeriesRepository.java
@@ -1,7 +1,12 @@
 package de.buggxs.mygarage.car.brand.db;
 
 import de.buggxs.mygarage.car.brand.ModelSeries;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ModelSeriesRepository extends PagingAndSortingRepository<ModelSeries, Long> {
+
+    Page<ModelSeries> findModelSeriesByNameContainingIgnoreCase(String name, Pageable pageable);
+
 }
