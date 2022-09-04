00aaaf98d8e36cb7af1af186df869287f4e93b60
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2022-09-04 20:53:08 +0200

    Rework jwt authentication

--- src/main/java/de/buggxs/mygarage/authentication/filter/JwtRequestFilter.java
+++ src/main/java/de/buggxs/mygarage/authentication/filter/JwtRequestFilter.java
@@ -36,7 +36,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
-            username = jwtUtil.extractUsername(jwt);
+            username = jwtUtil.getUsernameFromToken(jwt);
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
@@ -52,7 +52,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
-        
+
         filterChain.doFilter(request, response);
     }
 
--- src/main/java/de/buggxs/mygarage/authentication/utils/AuthTokenFilter.java
+++ src/main/java/de/buggxs/mygarage/authentication/utils/AuthTokenFilter.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -33,27 +34,37 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         try {
-            String jwt = parseJwt(request);
+            String jwtToken = parseJwt(request);
+            String username = null;
 
-            if (jwt != null && jwtUtil.validateJwtToken(jwt)) {
-                String username = jwtUtil.extractUsername(jwt);
-
-                org.springframework.security.core.userdetails.UserDetails springUserDetails =
-                        userDetailsService.loadUserByUsername(username);
-                
-                UsernamePasswordAuthenticationToken authenticationToken =
-                        new UsernamePasswordAuthenticationToken(
-                                springUserDetails, null, springUserDetails.getAuthorities()
-                        );
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            if (jwtToken != null) {
+                username = jwtUtil.getUsernameFromToken(jwtToken);
+            } else {
+                log.error("No token to validate.");
             }
 
+            // Once we get the token validate it.
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                // if token is valid configure Spring Security to manually set
+                // authentication
+                if (jwtUtil.validateToken(jwtToken, userDetails)) {
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails, null, userDetails.getAuthorities()
+                            );
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    // After setting the Authentication in the context, we specify
+                    // that the current user is authenticated. So it passes the
+                    // Spring Security Configurations successfully.
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
         } catch (Exception e) {
             log.error("Cannot set user authentication: ", e);
         }
-
         filterChain.doFilter(request, response);
     }
 
